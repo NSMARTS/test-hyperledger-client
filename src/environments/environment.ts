40964b8350e25e004ed3d7984ba2f29dbb1db7d4
ljwoon <ljwoon@nsmarts.co.kr>
2024-06-25 15:16:00 +0900

    fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
   tokenName: 'coop',
-  apiUrl: 'https://prod-domain.com/api/v1',
-  domain: 'prod-domain.com',
-  cmapURL: 'assets/lib/cmaps/',
-  workerSRC: 'assets/lib/build/pdf.worker.js'
+  apiUrl: 'http://public ip/api/v1',
+  domain: 'public ip',
+  cmapURL: '/assets/lib/cmaps/',
+  // workerSRC: '../../../../../assets/lib/build/pdf.worker.js',
+  workerSRC: '/assets/lib/build/pdf.worker.js',
 
 };
